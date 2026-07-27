1001ef6c8058181c8a2ff4d86e84f6e7e867e7a4
Crackwits <admin@crackwits.com>
2026-07-27 15:04:10 +0300

    Update PHONE_URL format in consts.js for international dialing: Changed phone number format to include the country code for improved accessibility.

--- utils/consts.js
+++ utils/consts.js
@@ -4,5 +4,5 @@ const TWITTER_URL = "https://x.com/BritamArabiaKSA";
 const LINKEDIN_URL = "https://www.linkedin.com/company/britam-arabia/";
 const YOUTUBE_URL = "https://www.youtube.com/@BritamArabiaKSA";
 const WHATSAPP_URL = 'https://wa.me/00966551765460';
-const PHONE_URL = "tel:009660138962802";
+const PHONE_URL = "tel:+9660138962802";
 export { FACEBOOK_URL, INSTAGRAM_URL, TWITTER_URL, LINKEDIN_URL, YOUTUBE_URL, WHATSAPP_URL, PHONE_URL };
